96b6ce43cd5a781784bcbc8ba49d73e0619cf4c7
samroska <samantha.roska@gmail.com>
2025-10-02 19:09:41 -0700

    forgot to remove localhost

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173", "https://bespoke-medovik-0b9d2c.netlify.app"],
+    allow_origins=[ "https://bespoke-medovik-0b9d2c.netlify.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
